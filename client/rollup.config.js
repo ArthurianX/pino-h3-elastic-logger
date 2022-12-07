28d8a6017d4c3e997865144334b30e7e84a5d7b4
Arthur Kovacs <kovacs.arthur.kraid@gmail.com>
2022-12-07 18:20:20 +0200

    Rollup behaves much better.

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,20 +1,38 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 
-const name = require('./package.json').main.replace(/\.js$/, '')
+const pack = require('./package.json')
+const name = pack.main.replace(/\.js$/, '')
+const version = `${pack.name} - ${pack.version}`
 
-const bundle = config => ({
+const bundle = (config) => ({
     ...config,
     input: 'src/index.ts',
-    external: id => !/^[./]/.test(id),
+    external: (id) => !/^[./]/.test(id),
 })
 
 export default [
     bundle({
-        plugins: [esbuild()],
+        plugins: [
+            esbuild({
+                minify: process.env.NODE_ENV === 'production',
+                define: {
+                    __LOGGER_VERSION__: JSON.stringify(version),
+                    'process.env.NODE_ENV': JSON.stringify(
+                        process.env.NODE_ENV
+                    ),
+                },
+            }),
+        ],
         output: [
             {
                 file: `${name}.js`,
+                format: 'umd',
+                sourcemap: true,
+                name: 'pinoLoggerClient',
+            },
+            {
+                file: `${name}.cjs`,
                 format: 'cjs',
                 sourcemap: true,
             },
